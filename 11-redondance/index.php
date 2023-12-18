1a9abb1baac94e8a2d7c69baa0ec313075fb9e07
Nicolas <nicolassegond0@gmail.com>
2023-12-18 11:59:51 +0100

    Ajout 10-11

---
+++ 11-redondance/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'Book.php';
+
+$physicalBook = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 42, "paper", 100, 1000000);
+$digitalBook = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 42, "digital", 0, 1000000);
