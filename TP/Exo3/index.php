6d9bfcd75fe905b00ba532a674fadbf81e2ec8db
Nicolas <nicolassegond0@gmail.com>
2023-12-19 08:33:53 +0100

    finalisation Exo3

--- TP/Exo3/index.php
+++ TP/Exo3/index.php
@@ -3,10 +3,12 @@
 require_once 'Fruits.php';
 require_once 'Panier.php';
 
+// Création des fruits
 $apple = new Fruits('Apple', 100, 1, 'https://apple.jpg');
 $cerise = new Fruits('Cerise', 150, 2, 'https://cerise.jpg');
 $banana = new Fruits('Banana', 150, 2, 'https://banana.jpg');
 
+// Création des paniers
 $panier1 = new Panier();
 $panier1->addPanier($apple);
 $panier1->addPanier($cerise);
@@ -16,26 +18,55 @@ $panier2 = new Panier();
 $panier2->addPanier($apple);
 
 $panier3 = new Panier();
-$panier3->addPanier($cerise);
+$panier3->addPanier($apple);
 
-$listPanier = array();
-$listPanier[] = $panier1;
-$listPanier[] = $panier2;
-$listPanier[] = $panier3;
+// Affichage des paniers
+print 'Panier 1:';
+foreach ($panier1->getPanier() as $fruit) {
+    print $fruit->__toString() . PHP_EOL;
+}
+
+print 'Panier 2:';
+foreach ($panier2->getPanier() as $fruit) {
+    print $fruit->__toString() . PHP_EOL;
+}
+
+// Formulaire de sélection de panier
 ?>
-
-<form action="" method="POST">
-    <select name="panier">
-        <?php foreach($listPanier as $panier) { ?>
-            <option value="<?php print $panier->__toString(); ?>"><?php print $panier->__toString(); ?></option>
-        <?php } ?>
-    </select>
-    <input type="submit" value="Valider">
-</form>
+    <form method="post" action="">
+        <label for="select_panier">Sélectionnez un panier :</label>
+        <select name="select_panier" id="select_panier">
+            <?php
+            // Génération dynamique des options
+            $paniers = [$panier1, $panier2, $panier3];
+            foreach ($paniers as $index => $panier) {
+                $fruitsNames = array_map(function ($fruit) {
+                    return $fruit->getName();
+                }, $panier->getPanier());
+                $optionText = "Panier " . ($index + 1) . ": " . implode(", ", $fruitsNames);
+                echo "<option value='panier$index'>$optionText</option>";
+            }
+            ?>
+        </select>
+        <input type="submit" value="Afficher le panier sélectionné">
+    </form>
 
 <?php
 
-if(isset($_POST['panier'])) {
-    print $_POST['panier'];
-}
+// Traitement du formulaire
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupération de la sélection du panier
+    $selectedPanierIndex = filter_input(INPUT_POST, 'select_panier', FILTER_SANITIZE_NUMBER_INT);
 
+    // Affichage du panier sélectionné
+    if (isset($paniers[$selectedPanierIndex])) {
+        $selectedPanier = $paniers[$selectedPanierIndex];
+        echo "Panier sélectionné (Panier " . ($selectedPanierIndex + 1) . "):";
+        foreach ($selectedPanier->getPanier() as $fruit) {
+            echo $fruit->__toString() . PHP_EOL;
+        }
+    } else {
+        echo "Panier non valide.";
+    }
+}
+?>
